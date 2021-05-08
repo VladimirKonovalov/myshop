1708e2a6ac2d048cccffa2286a25e0faf9877eaa
Vladimir Konvoalov <vlladimirkonovalov@gmail.com>
2021-05-08 20:36:49 +0300

    implement rating

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Card from "react-bootstrap/Card";
-import products from "../products";
+import Rating from './Rating'
 
 const Product = ({ product }) => {
     return (
@@ -15,11 +15,14 @@ const Product = ({ product }) => {
                         <strong>{product.name}</strong>
                     </Card.Title>
                 </a>
+
                 <Card.Text as='div'>
-                    <div className='my-3'>
-                        {product.rating} from {product.numReviews} reviews
-                    </div>
+                    <Rating
+                        value={product.rating}
+                        text={`${product.numReviews} reviews`}
+                    />
                 </Card.Text>
+
                 <Card.Text as='h3'>
                     ${product.price}
                 </Card.Text>
