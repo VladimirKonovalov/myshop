5275fc96e827127751b022311990ad95330917bd
Vladimir Konvoalov <vlladimirkonovalov@gmail.com>
2021-05-08 20:37:09 +0300

    implement header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Header from "./components/header";
 import Footer from "./components/footer"
 import HomeScreen from "./screens/HomeScreen"
+import Container from "react-bootstrap/Container";
 
 function App() {
     return (
         <>
             <Header/>
                 <main className='py-3'>
-                    <HomeScreen/>
+                    <Container>
+                        <HomeScreen/>
+                    </Container>
                 </main>
             <Footer/>
         </>
