fb8de5fac742dec838e857fc0b0e91fede2abdfc
Vladimir Konvoalov <vlladimirkonovalov@gmail.com>
2021-05-08 09:39:06 +0300

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from "./components/header";
+import Footer from "./components/footer"
+
+function App() {
+    return (
+        <>
+            <Header/>
+                <main>
+                    <h1>Welcome to MyShop</h1>
+                </main>
+            <Footer/>
+        </>
+    );
+}
+
+export default App;
---
+++ frontend/src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <footer>
+            footer
+        </footer>
+    );
+}
+
+export default Header
