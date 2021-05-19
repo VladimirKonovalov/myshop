6432b6f52aad5e6bac9138db97ec22ab578f4a5f
Vladimir Konvoalov <vlladimirkonovalov@gmail.com>
2021-05-19 09:26:21 +0300

    add custom error handler

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -18,19 +18,17 @@ router.get('/', asyncHandler(async (req, res) => {
 // @access  Public
 router.get(
     '/:id',
-    async(req, res) => {
-        try {
+    asyncHandler(async(req, res) => {
             const product = await Product.findById(req.params.id)
 
             if (product) {
                 res.json(product)
             } else {
-                res.status(404).json({message: 'Product not found'})
+                res.status(404)
+                //json({message: 'Product not found'})
+                throw new Error('Product not found')
             }
-        } catch (e) {
-            res.status(404).json({message: 'Product not found'})
-        }
-    }
+    })
 )
 
 export  default router
