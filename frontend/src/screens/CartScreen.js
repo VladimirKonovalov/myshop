6517fed5a12c685a7cf106960c0e3d5e9ab523e0
Vladimir Konvoalov <vlladimirkonovalov@gmail.com>
2021-06-11 15:50:13 +0300

    add frontend user login logic

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -82,8 +82,7 @@ const CartScreen = ({ match, location, history }) => {
                             <h2>
                                 Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)}) items
                             </h2>
-                            ${cartItems.reduce((acc, item) => acc + item.qty * item.price, 0).
-                            toFixed(2)}
+                            ${cartItems.reduce((acc, item) => acc + item.qty * item.price, 0).toFixed(2)}
                         </ListGroup.Item>
                         <ListGroup.Item>
                             <Button type='button' className='btn-block' disabled={cartItems.length === 0}
